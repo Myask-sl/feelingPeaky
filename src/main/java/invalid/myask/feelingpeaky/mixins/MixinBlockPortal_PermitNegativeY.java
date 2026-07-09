68c7839f6281958724f2b2b59939cce386e7fc4f
Myask-sl <208960411+Myask-sl@users.noreply.github.com>
2026-07-09 16:03:33 -0600

    attempt to make portals work

---
+++ src/main/java/invalid/myask/feelingpeaky/mixins/MixinBlockPortal_PermitNegativeY.java
@@ -0,0 +1,23 @@
+package invalid.myask.feelingpeaky.mixins;
+
+import net.minecraft.block.BlockPortal;
+import net.minecraft.world.World;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import invalid.myask.feelingpeaky.ducks.IExpandedWorldOrProvider;
+
+@Mixin(BlockPortal.class)
+public class MixinBlockPortal_PermitNegativeY {
+    @Definition(id = "l", local = @Local(type = int.class, name = "l"))
+    @Expression("l > 0")
+    @ModifyExpressionValue(method = "updateTick", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean aboveWorldBottom (boolean original, World world, int x, int y) {
+        return y > ((IExpandedWorldOrProvider)world).getWorldMinY();
+    }
+}
